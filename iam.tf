b75ad27573867ac0776165b946f8e36af2bd7552
nwamanu <nnabuchimanu@yahoo.com>
2022-08-17 19:54:58 -0600

    new iam changes

---
+++ iam.tf
@@ -0,0 +1,9 @@
+klllllllllllllllllllllllllllll
+kdkidijg'developmentsa
+kgcccccccccccccccccccc
+sfidlgidagdadfdafdad
+sfdadggadfafeitodifdaad
+asdfdioadn gad0dad
+dagdadadgadgasdada "name"{
+    
+}
